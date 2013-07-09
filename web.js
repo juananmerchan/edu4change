ebc63af03317e30ccdb3a35fb89b46a43812eefa
juanan <juanan@juanan-VirtualBox.(none)>
2013-07-09 10:26:33 +0200

    Add web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile(index.html);
+  fs.readFile('index.html', 'utf8',function (err, data) {
+  if (err) throw err;
+  console.log(data);	
+ });
 });
 
 var port = process.env.PORT || 5000;
