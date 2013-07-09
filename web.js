5ee03f7281d6bc729740bbe982a9fcf7ba875fb8
Juanan <juanan.merchan@gmail.com>
2013-07-09 12:13:58 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
+var homepage = fs.readFileSync('index.html');
+var buf = new Buffer(homepage);                                              
+buf.toString('utf8', 0, buf.length);                                  
+});              
 
 app.get('/', function(request, response) {
-  response.end('Hi there!');
+  response.send(buf);
+  //response.end('Hi there!');
   /*fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
