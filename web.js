19bd1f4b300a275638c60e724b367c28ebaba8a5
Juanan <juanan.merchan@gmail.com>
2013-07-09 11:00:13 +0200

    Update web.js

--- web.js
+++ web.js
@@ -3,10 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', 'utf8',function (err, data) {
-  if (err) throw err;
-  console.log(data);	
- });
+  response.send('Hello World');
 });
 
 var port = process.env.PORT || 5000;
